0adf151c760b7fc58e6b6853d2567817e1f1324e
Logstor <a.rottger_rydahl@live.dk>
2021-03-26 16:06:57 +0100

    Updated TODO and Initial implementation of EndpointController

---
+++ TODO.md
@@ -0,0 +1,5 @@
+### Tasks
+ - Implement EndpointController
+    - Write docs for DeleteEndpoint()
+
+ - Implement proper SIGINT/SIGKILL handling
---
+++ src/EndpointController.cs
@@ -0,0 +1,118 @@
+using System;
+using DuetAPIClient;
+using DuetAPI;
+using DuetAPI.Commands;
+using DuetAPI.Connection;
+using DuetAPI.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Meminisse
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EndpointController
+    {
+        /// <summary>
+        /// Indicates whether the controller instance has been initialized.
+        /// </summary>
+        /// <value></value>
+        public bool isInitialized { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private CommandConnection connection;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Dictionary<int, HttpEndpointUnixSocket> endpoints;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private int currKey = 0;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private string socketPath;
+
+        public EndpointController(string socketPath = Defaults.FullSocketPath)
+        {
+            this.connection = new();
+            this.socketPath = socketPath;
+            this.endpoints = new();
+        }
+
+        /// <summary>
+        /// Makes the controller connect to the Controlserver, which is required before any operation can be made.
+        /// </summary>
+        /// 
+        /// <returns>void</returns>
+        /// <exception cref="DuetAPI.Connection.IncompatibleVersionException"></exception>
+        /// <exception cref="System.IO.IOException"></exception>
+        /// <exception cref="System.OperationCanceledException"></exception>
+        /// <exception cref="System.Net.Sockets.SocketException"></exception>
+        public async void Init()
+        {
+            // Make sure not to initialize again.
+            if (this.isInitialized) return;
+
+            await this.connection.Connect(this.socketPath);
+            this.isInitialized = true;
+        }
+
+        /// <summary>
+        /// Cleaning up all the remaining HttpEndpoints and Connections.
+        /// </summary>
+        public void CleanUp()
+        {
+            if (this.isInitialized && this.connection.IsConnected)
+            {
+                List<Task> tasks = new();
+
+                // Run over every Endpoint and remove it
+                foreach ((int key, HttpEndpointUnixSocket endpoint) in this.endpoints)
+                    tasks.Add(this.connection.RemoveHttpEndpoint(endpoint.EndpointType, endpoint.Namespace, endpoint.EndpointPath));
+
+                // Busy wait
+                foreach (Task task in tasks)
+                    task.Wait();
+
+                // Close the connection
+                this.connection.Close();
+            }
+        }
+
+        public async Task<(int, HttpEndpointUnixSocket)> CreateEndpoint(string ns, string path)
+        {
+            // Add the endpoint
+            HttpEndpointUnixSocket socket = await this.connection.AddHttpEndpoint(HttpEndpointType.GET, ns, path, false, backlog: 4);
+
+            // Add to the Dictionary
+            int key = this.currKey++;
+            this.endpoints.Add(key, socket);
+
+            return (key, socket);
+        }
+
+        public async void DeleteEndpoint(int key)
+        {
+            // Retrieve from dictionary
+            HttpEndpointUnixSocket socket;
+            bool success = this.endpoints.TryGetValue(key, out socket);
+
+            // If it exists remove the connection
+            if (success)
+                await this.connection.RemoveHttpEndpoint(socket.EndpointType, socket.Namespace, socket.EndpointPath);
+        }
+    }
+}
